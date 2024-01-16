975844816e584754f9182dde988f6d2fbacb2bde
hakugakusha <87151999+hakugakusha@users.noreply.github.com>
2024-01-16 01:43:31 -0500

    finished display regions

--- src/Driver.py
+++ src/Driver.py
@@ -1,7 +1,19 @@
 from main.view.Display import Display
 
+continent_url = 'https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/continents.json'
+country_url = 'https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/countries.json' 
+
 def launch():
-    Display.startApp()
+    display = Display()
+
+    display.startApp()
+
+    display.getRegion(continent_url)
+    display.getRegion(country_url)
+    display.showRegions()
+    
+    
 
 if __name__ == '__main__':
     launch()
+
--- src/main/view/Display.py
+++ src/main/view/Display.py
@@ -1,8 +1,42 @@
+import requests
+
 class Display:
-    def startApp():
+    def __init__(self):
+        self.regions = []
+        self.storeData = []
+
+    def startApp(self):
         print("Hello! Welcome to WCA Rankings!")
         print("This is a joint-project by Brooklyn Schmidt and YuFeng Lin")
 
+    def getRegion(self, url):
+
+        rank_url = url.format()
+
+
+        # Make the GET request
+        result = requests.get(rank_url)  
+
+        if result.status_code == 200:
+            result = result.json()
+            results_by_competition_and_event = result['items']
+
+            for entry in results_by_competition_and_event:
+                self.storeData.append(entry)
+
+            for region in self.storeData:
+                current_name = region['name']
+                if (current_name[0:8] != "Multiple"):
+                    self.regions.append(region['name'])
+
+            self.storeData = []
+
+    def showRegions(self):
+        print(self.regions)
+
+
+
+    
 
 
 
