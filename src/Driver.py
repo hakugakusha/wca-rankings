ecaddc2c872294f13fd800f2d0ca5443f49c3d08
hakugakusha <87151999+hakugakusha@users.noreply.github.com>
2024-01-16 00:49:10 -0500

    driver implementation

---
+++ src/Driver.py
@@ -0,0 +1,7 @@
+from main.view.Display import Display
+
+def launch():
+    Display.startApp()
+
+if __name__ == '__main__':
+    launch()
---
+++ src/main/view/Display.py
@@ -0,0 +1,8 @@
+class Display:
+    def startApp():
+        print("Hello! Welcome to WCA Rankings!")
+        print("This is a joint-project by Brooklyn Schmidt and YuFeng Lin")
+
+
+
+
